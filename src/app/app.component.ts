e11d43c61247432030c4516fb71892686a36faeb
Driarake <adriana_drix@yahoo.com.br>
2022-07-12 22:40:08 -0300

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-first-project';
+export class AppComponent implements OnInit {
+  count = 0;
+  nome = 'Adriana';
+  text: string = '';
+
+  pessoas = [
+    {
+      nome: "Adriana",
+      sobrenome: "Arake"
+    },
+    {
+      nome: "Maria",
+      sobrenome: "Silva"
+    },
+    {
+      nome: "João",
+      sobrenome: "Pereira"
+    },
+    {
+      nome: "Marcio",
+      sobrenome: "Santos"
+    }
+  ];
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    console.log(this.pessoas);
+    let interval = setInterval(() => {
+      this.count++;
+      if (this.count === 10) {
+        clearInterval(interval);
+      }
+    }, 1000)
+  }
+
+  clicou(nome: string): void {
+    console.log('Clicou em mim', nome)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ButtonComponent } from './components/button/button.component';
+import { MarcaTextoDirective } from './directives/marca-texto.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ButtonComponent
+    MarcaTextoDirective,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
